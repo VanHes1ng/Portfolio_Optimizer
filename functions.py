f8ac867624f388e877390a277f5a5f949afd4fbc
VanHelsing <ivan.kochergat@gmail.com>
2024-06-20 10:13:52 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,20 +5,20 @@ import streamlit as st
 
 class portfolio_optimization:
     def sharpe(src): 
-        d_r = src.pct_change()
-        std = d_r.std()
-        mean = d_r.mean()
+        d_r     = src.pct_change()
+        std     = d_r.std()
+        mean    = d_r.mean()
         return (mean / std) * np.sqrt(365)
 
 
     def sortino(src):
-        d_r = src.pct_change()
+        d_r      = src.pct_change()
         stdv_neg = (d_r[d_r < 0]*-1).std()
-        mean = d_r.mean()
+        mean     = d_r.mean()
         return (mean / stdv_neg) * np.sqrt(365)
 
     def omega(src):
-        d_r = src.pct_change()
+        d_r      = src.pct_change()
         stdv_neg = (d_r[d_r < 0]*-1).sum()
         stdv_pos = (d_r[d_r > 0]).sum()
         return (stdv_pos / stdv_neg) 
@@ -26,16 +26,16 @@ class portfolio_optimization:
 
     def max_drawdown(src):
         return_series = src.pct_change()
-        comp_ret = (return_series+1).cumprod()
-        peak = comp_ret.expanding(min_periods=1).max()
-        dd = (comp_ret/peak)-1
+        comp_ret      = (return_series+1).cumprod()
+        peak          = comp_ret.expanding(min_periods=1).max()
+        dd            = (comp_ret/peak)-1
         return dd.min()
     
     def rol_max_drawdown(src):
         return_series = src.pct_change()
-        comp_ret = (return_series+1).cumprod()
-        peak = comp_ret.expanding(min_periods=1).max()
-        dd = (comp_ret/peak)-1
+        comp_ret      = (return_series+1).cumprod()
+        peak          = comp_ret.expanding(min_periods=1).max()
+        dd            = (comp_ret/peak)-1
         return dd
 
     def daily_vol(src):
@@ -43,12 +43,12 @@ class portfolio_optimization:
         return d_r.std()
 
     def plot_ef(n_samples, ef, label):
-        w = np.random.dirichlet(np.ones(ef.n_assets), n_samples)
-        rets = w.dot(ef.expected_returns)
-        stds = np.sqrt(np.diag(w @ ef.cov_matrix @ w.T))
-        sharpes = rets / stds
+        w               = np.random.dirichlet(np.ones(ef.n_assets), n_samples)
+        rets            = w.dot(ef.expected_returns)
+        stds            = np.sqrt(np.diag(w @ ef.cov_matrix @ w.T))
+        sharpes         = rets / stds
         plt.style.use("_classic_test_patch")
-        fig, ax = plt.subplots(figsize=(15,7))
+        fig, ax         = plt.subplots(figsize=(15,7))
         ax.scatter(stds, rets, marker=".", c=sharpes, cmap="winter")
 
         # Portfolio
